54cc83ce4a1a2a0903c8312a6c5867d4bda236ce
Pierre-alexandre Montiel <pmontiel@c1r7p2.42nice.fr>
2022-03-21 11:39:23 +0100

    qqqq

--- srcs/requirements/mariadb/tools/script.sh
+++ srcs/requirements/mariadb/tools/script.sh
@@ -1,18 +1,12 @@
 #!/bin/sh
-#if [ ! -d "/var/lib/mysql/wordpress" ]; then
-#	mysql_install_db
-	service mysql start
-	#service mysql start
-	sleep 2
-	echo "CREATE DATABASE wordpress;" | mysql
-	echo "CREATE USER 'pa'@'%' identified by 'pa42';" | mysql
-	echo "GRANT ALL PRIVILEGES ON *.* TO 'pa'@'%';" | mysql
-	echo "FLUSH PRIVILEGES;" | mysql
-	service mysql stop
-#fi
-	service mysql start
-	mysql wordpress < ./srcs/database.sql
-#mysql
-#service mysql stop
-#mysql_safe
-#sleep infinity
+service mysql start
+sleep 2
+echo "CREATE DATABASE wordpress;" | mysql
+echo "CREATE USER 'pa'@'%' identified by 'pa42';" | mysql
+echo "GRANT ALL PRIVILEGES ON *.* TO 'pa'@'%';" | mysql
+echo "FLUSH PRIVILEGES;" | mysql
+service mysql stop
+service mysql start
+mysql wordpress < ./srcs/database.sql
+service mysql stop
+mysqld_safe
